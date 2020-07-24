b8548f50891a122d98b1f3f82ed0a94bdef1cc76
E.Kouna <kounagael@gmail.com>
2020-07-24 17:46:54 +0100

    implement login page front-end

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -3,7 +3,6 @@
     <router-view/>
   </div>
 </template>
-
 <script>
 export default {
   name: 'App'
@@ -12,5 +11,32 @@ export default {
 <style lang="scss">
 html {
   font-size: 14px;
+}
+
+public.container {
+  max-width: 900px;
+}
+.public {
+  .form {
+    margin-top: 50px;
+    width: 320px;
+    .form-group {
+      label {
+        font-weight: bold;
+        color: #555;
+      }
+      .error {
+        line-height: 1;
+        display: none;
+        margin-top: 5px;
+      }
+    }
+  }
+}
+.field-error {
+  .error {
+    display: block;
+    color: #ff0000;
+  }
 }
 </style>
